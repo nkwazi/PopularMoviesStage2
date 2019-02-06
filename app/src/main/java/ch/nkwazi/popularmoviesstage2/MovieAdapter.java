c2b0d514242af0f7f478b19899d40db1c9733ad3
nkwazi <janekdekock@protonmail.com>
2019-02-06 15:39:19 +0100

    refactored to elimante redunceny such as Interface onClickListener

--- app/src/main/java/ch/nkwazi/popularmoviesstage2/MovieAdapter.java
+++ app/src/main/java/ch/nkwazi/popularmoviesstage2/MovieAdapter.java
@@ -1,6 +1,7 @@
 package ch.nkwazi.popularmoviesstage2;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -18,33 +19,38 @@ import java.util.List;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
 
-    private static final String TAG = MovieAdapter.class.getSimpleName();
-
-    private MovieAdapterOnClickHandler movieAdapterOnClickHandler;
+    private Context context;
     private List<Movie> movieList;
 
-    public interface MovieAdapterOnClickHandler {
-        void onClick(Movie movie);
+    public MovieAdapter(Context mContext, List<Movie> movieList) {
+        this.context = mContext;
+        this.movieList = movieList;
     }
 
-    MovieAdapter(MovieAdapterOnClickHandler mClickHandler){
-        this.movieAdapterOnClickHandler = mClickHandler;
-    }
+    public class MovieViewHolder extends RecyclerView.ViewHolder {
 
-    public class MovieViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public final ImageView myImageView;
 
         MovieViewHolder(View view){
             super(view);
             myImageView = view.findViewById(R.id.coverPicture);
-            view.setOnClickListener(this);
-        }
 
-        @Override
-        public void onClick(View v) {
-            int adapterPosition = getAdapterPosition();
-            Movie selectedMovie = movieList.get(adapterPosition);
-            movieAdapterOnClickHandler.onClick(selectedMovie);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = getAdapterPosition();
+                    if(pos != RecyclerView.NO_POSITION){
+                        Intent intent = new Intent(v.getContext(), DetailActivity.class);
+
+                        Movie currentMovie = movieList.get(pos);
+
+                        intent.putExtra(Movie.TAG, currentMovie);
+                        intent.putExtra(DetailActivity.TAG, currentMovie);
+
+                        v.getContext().startActivity(intent);
+                    }
+                }
+            });
         }
     }
 
@@ -74,8 +80,8 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
         return movieList.size();
     }
 
-    public void setMovieList(List<Movie> movieList) {
-        this.movieList = movieList;
+    public void setMovieList(List<Movie> movie) {
+        movieList = movie;
         notifyDataSetChanged();
     }
 }
